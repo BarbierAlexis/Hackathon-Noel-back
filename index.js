d79148de5b4cdd4096fe0f733d4924bceb78e0ed
Celine Berthelin <celine.berthelin@gmail.com>
2019-12-19 17:53:20 +0100

    back router

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 
 const express = require ('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors')
 
 const tweetsRouter = require ('./routes/tweet')
 const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cors())
 app.use('/tweet', tweetsRouter);
+app.use(axios)
 
 app.listen(8000, () => {
 console.log('Server running')
--- routes/tweet.js
+++ routes/tweet.js
@@ -1,13 +1,21 @@
 const express = require('express');
-
 const router = express.Router();
 
+console.log('sdjsqjdlksqdsqkj')
 // router.get('/', (req, res) => {
 //   res.send('Bonjour VUE.JS');
 // });
 
 router.post('/', (req, res) => {
-  res.send(`${req.body.description}, ${req.body.dechet}`)
+  axios.post(`https://twitter.com/${}`, {
+    description: req.body.description,
+    dechet: req.body.dechet,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude
+  })
+  res.sendStatus(200);
+
 });
+
 
 module.exports = router;
