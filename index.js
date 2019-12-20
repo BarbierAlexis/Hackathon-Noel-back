00217d91eb152758ecf784bb5e9781cd4b33be54
Celine Berthelin <celine.berthelin@gmail.com>
2019-12-20 12:47:10 +0100

    Twitter and Form join

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const express = require ('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const cors = require('cors')
-
-const tweetsRouter = require ('./routes/tweet')
 const app = express();
+
+const tweetsRouter = require('./routes/tweet.js')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -14,6 +14,6 @@ app.use(cors())
 app.use('/tweet', tweetsRouter);
 app.use(axios)
 
-app.listen(8000, () => {
+app.listen(8090, () => {
 console.log('Server running')
 });
--- routes/tweet.js
+++ routes/tweet.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-console.log('sdjsqjdlksqdsqkj')
-// router.get('/', (req, res) => {
-//   res.send('Bonjour VUE.JS');
-// });
+const Twit = require ('twit')
+
+const T = new Twit ({
+  consumer_key: '71TUkzS2JvIk73stRJiunEp4K',
+  consumer_secret: '9T2yY9lq1JarKzp412k9f4mwPu4pgQLBXUFueOXsCj2BvyVp7X',
+  access_token: '1207336111477051394-rdfC1vkaM348UXhLivNQ9uGGSBkaoi',
+  access_token_secret: 'E9puHsBjB579pM82Hpwqq8G3kY9a4nEj7gqh1oVouXR1m'
+})
+
+
+
+
+console.log('toto')
 
 router.post('/', (req, res) => {
-  axios.post(`https://twitter.com/${}`, {
-    description: req.body.description,
-    dechet: req.body.dechet,
-    latitude: req.body.latitude,
-    longitude: req.body.longitude
-  })
-  res.sendStatus(200);
-
+  T.post('/statuses/update', { status: `${ JSON.stringify(req.body) }` }, (err, data, response) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      console.log(req.body)
+      res.sendStatus(200);
+    }
+  });
 });
-
 
 module.exports = router;
